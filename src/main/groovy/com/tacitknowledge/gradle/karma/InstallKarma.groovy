0ee0130554718591d298bb890198210f7edfdf30
Ivan Frolov <ifrolov@tacitknowledge.com>
2016-02-22 00:36:14 +1100

    PAAS-000 Better locking routine

--- src/main/groovy/com/tacitknowledge/gradle/karma/InstallKarma.groovy
+++ src/main/groovy/com/tacitknowledge/gradle/karma/InstallKarma.groovy
@@ -11,7 +11,7 @@ class InstallKarma extends NpmTask
 
     project.afterEvaluate{
       workingDir = project.node.nodeModulesDir
-      
+
       def pkgName = project.karma.version ? "karma@${project.karma.version}" : 'karma'
       args = ['install', pkgName]
       args += project.karma.additionalNodeDependencies?.collect { k,v -> "$k@$v" } ?: [:]
@@ -35,15 +35,17 @@ class InstallKarma extends NpmTask
     def random = new RandomAccessFile(new File(npmUserFolder, 'node_modules_lock'), 'rw')
     def lock = null
 
-    for(i in (1..3)){
-     try {
-       logger.info("Trying to acquire lock to install karma. Attampt nr $i.")
-       lock = random.channel.tryLock()
-       break;
-     } catch (Exception) {
-       logger.warn("Can't acquire lock. Waiting 60s before attempt nr ${i+1}.")
-       sleep(60000)
-     }
+    for (int i = 0; i < 3; i++) {
+      try {
+        logger.info("Trying to acquire lock to install karma. Attampt nr $i.")
+        lock = random.channel.tryLock()
+      } catch (ignore) { /*noop*/ }
+      if(lock) {
+        break
+      } else {
+        logger.warn('Sleeping before the next attempt')
+        sleep(60000)
+      }
     }
 
     if(!lock) {
